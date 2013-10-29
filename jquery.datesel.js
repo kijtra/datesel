23e05419105cc3c9b32ca4a91d0cae2e6cd0d384
kijtra <kijtra@gmail.com>
2013-10-29 17:06:36 +0900

    最大・最小設定を調整

--- jquery.datesel.js
+++ jquery.datesel.js
@@ -4,7 +4,8 @@
 			formatDate:'y-m-d',
 			formatTime:'h:i',
 			minuteStep:5,
-			className:'form-control',
+			wrapClass:'datesel-wrap',
+			selectClass:'form-control',
 			week:true,
 			weekName:new Array('日','月','火','水','木','金','土')
 		},conf);
@@ -15,13 +16,9 @@
 		nowM=now.getMonth()+1,
 		nowD=now.getDate(),
 		nowH=now.getHours(),
-		nowI=now.getMinutes();
-
-		var normalize=function(str){
-			return str.replace(/[Ａ-Ｚａ-ｚ０-９－／　]/g, function(s){
-				return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-			});
-		};
+		nowI=now.getMinutes(),
+		minDY=nowY-3,
+		maxDY=nowY+1;
 
 		var dec=function(dt,max){
 			if(!dt || 'string'!=typeof(dt)){
@@ -84,9 +81,9 @@
 		};
 
 		var oY=(function(){
-			var html='<select class="'+conf.className+'">';
+			var html='<select class="'+conf.selectClass+'">';
 			html+='<option value="">年</option>';
-			for(var i=nowY-3,l=nowY+1;i<l;i++){
+			for(var i=minDY,l=maxDY;i<l;i++){
 				html+='<option value="'+i+'">'+i+'年</option>';
 			}
 			html+='</select>';
@@ -94,7 +91,7 @@
 		})(),
 
 		oM=(function(){
-			var html='<select class="'+conf.className+'">';
+			var html='<select class="'+conf.selectClass+'">';
 			html+='<option value="">月</option>';
 			for(var i=1,l=12;i<l;i++){
 				html+='<option value="'+i+'">'+((' '+i).slice(-2))+'月</option>';
@@ -104,7 +101,7 @@
 		})(),
 
 		oD=(function(){
-			var html='<select class="'+conf.className+'">';
+			var html='<select class="'+conf.selectClass+'">';
 			html+='<option value="">日</option>';
 			if(conf.week && conf.weekName){
 				var wk=conf.weekName;
@@ -122,7 +119,7 @@
 		})(),
 
 		oH=(function(){
-			var html='<select class="'+conf.className+'">';
+			var html='<select class="'+conf.selectClass+'">';
 			html+='<option value="">時</option>';
 			for(var i=0,l=23;i<=l;i++){
 				html+='<option value="'+i+'">'+((' '+i).slice(-2))+'時</option>';
@@ -132,7 +129,7 @@
 		})(),
 
 		oI=(function(){
-			var step=conf.minuteStep,html='<select class="'+conf.className+'">';
+			var step=conf.minuteStep,html='<select class="'+conf.selectClass+'">';
 			html+='<option value="">分</option>';
 			for(var i=0,l=59;i<=l;i=i+step){
 				html+='<option value="'+i+'">'+(('0'+i).slice(-2))+'分</option>';
@@ -141,24 +138,31 @@
 			return $(html);
 		})();
 
-		var getCurrent=function(t){
-			var y,m,d,h,i,v,data=t.data(),ret={y:null,m:null,d:null,h:null,i:null};
+		var getCurrent=function(t,toNum){
+			var y,m,d,h,i,v,str='',data=t.data(),ret={y:null,m:null,d:null,h:null,i:null};
 			if(data.y){
 				v=data.y.val();
 				if(v){
 					ret.y=v;
+					str+=v;
 				}
 			}
 			if(data.m){
 				v=data.m.val();
 				if(v){
 					ret.m=v;
+					if(toNum && ret.y){
+						str+=('0'+v).slice(-2);
+					}
 				}
 			}
 			if(data.d){
 				v=data.d.val();
 				if(v){
 					ret.d=v;
+					if(toNum && ret.m){
+						str+=('0'+v).slice(-2);
+					}
 				}
 			}
 			if(data.h){
@@ -173,7 +177,21 @@
 					ret.i=v;
 				}
 			}
-			return ret;
+
+			if(toNum){
+				if(!str || str.length<8){
+					return false;
+				}
+				if(ret.h){
+					str+=('0'+ret.h).slice(-2);
+					if(ret.i){
+						str+=('0'+ret.i).slice(-2);
+					}
+				}
+				return Number(str);
+			}else{
+				return ret;
+			}
 		},
 
 		setValue=function(t){
@@ -192,7 +210,7 @@
 				}
 			}
 
-			if(ret.y && ret.m && ret.d){
+			if(ret && ret.y && ret.m && ret.d){
 				var f=data.formatDate;
 				var r=f.replace('y',ret.y).replace('m',ret.m).replace('d',ret.d);
 				if(data.formatTime && ret.h && ret.i){
@@ -205,17 +223,19 @@
 		};
 
 		var setY=function(t,selected){
-			var min,max,html='',o=t.data('y');
+			var min,max,minO,maxO,val,html='',o=t.data('y');
 			if(!selected){
 				selected=o.val();
 			}
+
 			if(!t.data('min')){
-				min=nowY-3;
+				min=minDY;
 			}else{
 				min=t.data('min').date.getFullYear();
 			}
+
 			if(!t.data('max')){
-				max=nowY+1;
+				max=maxDY;
 			}else{
 				max=t.data('max').date.getFullYear();
 			}
@@ -271,7 +291,7 @@
 			for(var i=1,l=12;i<=l;i++){
 				var s=(selected==i ? ' selected="selected"' : '');
 				var d=(i<min || i>max ? ' disabled="disabled"' : '');
-				html+='<option value="'+i+'"'+s+''+d+'>'+((' '+i).slice(-2))+'月</option>';
+				html+='<option value="'+i+'"'+s+d+'>'+((' '+i).slice(-2))+'月</option>';
 			}
 			o.html(html);
 		},
@@ -323,7 +343,7 @@
 				dM=dat.getMonth()+1;
 				if(current){
 					if(current.y>dY && current.m>dM){
-						max=60;console.log(max);
+						max=60;
 						selected=null;
 						o.val('');
 					}else if(current.y==dY && current.m==dM){
@@ -343,95 +363,286 @@
 					var w=wk[(new Date(tY,tM-1,i)).getDay()];
 					var s=(selected==i ? ' selected="selected"' : '');
 					var d=(i<min || i>max ? ' disabled="disabled"' : '');
-					html+='<option value="'+i+'"'+s+''+d+'>'+((' '+i).slice(-2))+'日('+w+')</option>';
+					html+='<option value="'+i+'"'+s+d+'>'+((' '+i).slice(-2))+'日('+w+')</option>';
 				}
 			}else{
 				for(var i=1,l=len;i<=l;i++){
 					var s=(selected==i ? ' selected="selected"' : '');
 					var d=(i<min || i>max ? ' disabled="disabled"' : '');
-					html+='<option value="'+i+'"'+s+''+d+'>'+((' '+i).slice(-2))+'日</option>';
+					html+='<option value="'+i+'"'+s+d+'>'+((' '+i).slice(-2))+'日</option>';
 				}
 			}
 			o.html(html);
 		},
 
 		setH=function(t,selected){
-			var html='',o=t.data('h');
+			var min=0,max=23,html='',o=t.data('h'),
+			current=getCurrent(t,true),
+			curDate=(current ? Number((''+current).substr(0,8)) : null);
+
+			if(!o){
+				return false;
+			}
 			if(!selected){
 				selected=o.val();
+			}
+
+			if(t.data('min')){
+				if(current){
+					var dat=t.data('min').date,
+					dY=dat.getFullYear(),
+					dM=dat.getMonth()+1,
+					dD=dat.getDate(),
+					dH=dat.getHours(),
+					curMin=Number(dY+(('0'+dM).slice(-2))+(('0'+dD).slice(-2)));
+					if(curDate==curMin){
+						min=dH;
+					}else if(curDate<curMin){
+						min=24;
+						selected=null;
+					}
+				}else{
+					min=24;
+					selected=null;
+				}
+			}
+
+			if(t.data('max')){
+				if(current){
+					var dat=t.data('max').date,
+					dY=dat.getFullYear(),
+					dM=dat.getMonth()+1,
+					dD=dat.getDate(),
+					dH=dat.getHours(),
+					curMax=Number(dY+(('0'+dM).slice(-2))+(('0'+dD).slice(-2)));
+					if(curDate==curMax){
+						max=dH;
+					}else if(curDate>curMax){
+						max=0;
+						selected=null;
+					}
+				}else{
+					max=0;
+					selected=null;
+				}
 			}
 
 			html+='<option value="">時</option>';
 			for(var i=0,l=23;i<=l;i++){
 				var s=(selected==i ? ' selected="selected"' : '');
-				html+='<option value="'+i+'"'+s+'>'+((' '+i).slice(-2))+'時</option>';
+				var d=(i<min || i>max ? ' disabled="disabled"' : '');
+				html+='<option value="'+i+'"'+s+d+'>'+((' '+i).slice(-2))+'時</option>';
 			}
 
 			o.html(html);
 		},
 
 		setI=function(t,selected){
-			var html='',step=conf.minuteStep,o=t.data('i');
+			var min=0,max=59,html='',step=conf.minuteStep,o=t.data('i'),
+			current=getCurrent(t,true),
+			curTime=(current ? Number((''+current).substr(0,10)) : null);
+
+			if(!o){
+				return false;
+			}
+
 			if(!selected){
 				selected=o.val();
+			}
+
+			if(t.data('min')){
+				if(current){
+					var dat=t.data('min').date,
+					dY=dat.getFullYear(),
+					dM=dat.getMonth()+1,
+					dD=dat.getDate(),
+					dH=dat.getHours(),
+					dI=dat.getMinutes(),
+					curMin=Number(dY+(('0'+dM).slice(-2))+(('0'+dD).slice(-2))+(('0'+dH).slice(-2)));
+					if(curTime==curMin){
+						min=dI;
+					}else if(curTime<curMin){
+						min=60;
+						selected=null;
+					}
+				}else{
+					min=60;
+					selected=null;
+				}
+			}
+
+			if(t.data('max')){
+				if(current){
+					var dat=t.data('max').date,
+					dY=dat.getFullYear(),
+					dM=dat.getMonth()+1,
+					dD=dat.getDate(),
+					dH=dat.getHours(),
+					dI=dat.getMinutes(),
+					curMax=Number(dY+(('0'+dM).slice(-2))+(('0'+dD).slice(-2))+(('0'+dH).slice(-2)));
+					if(curTime==curMax){
+						max=dI;
+					}else if(curTime>curMax){
+						max=0;
+						selected=null;
+					}
+				}else{
+					max=0;
+					selected=null;
+				}
 			}
 
 			html+='<option value="">分</option>';
 			for(var i=0,l=59;i<=l;i=i+step){
 				var s=(selected==i ? ' selected="selected"' : '');
-				html+='<option value="'+i+'"'+s+'>'+(('0'+i).slice(-2))+'分</option>';
+				var d=(i<min || i>max ? ' disabled="disabled"' : '');
+				html+='<option value="'+i+'"'+s+d+'>'+(('0'+i).slice(-2))+'分</option>';
 			}
 
 			o.html(html);
 		};
 
+		var refresh=function(t,ignore){
+			var c,o,v;
+			if(t.data('minObj')){
+				o=t.data('minObj');
+				v=getCurrent(t);
+				v.y=(v.y ? v.y : maxDY);
+				v.m=(v.m ? v.m : 12);
+				v.d=(v.d ? v.d : 31);
+				v.h=(v.h ? v.h : 23);
+				v.i=(v.i ? v.i : 59);
+				o.data('max',dec(v.y+'-'+v.m+'-'+v.d+' '+v.h+':'+v.i));
+
+				v=getCurrent(o);
+				v.y=(v.y ? v.y : minDY);
+				v.m=(v.m ? v.m : '01');
+				v.d=(v.d ? v.d : '01');
+				v.h=(v.h ? v.h : '00');
+				v.i=(v.i ? v.i : '00');
+				t.data('min',dec(v.y+'-'+v.m+'-'+v.d+' '+v.h+':'+v.i));
+			}else if(t.data('maxObj')){
+				o=t.data('maxObj');
+				v=getCurrent(t);
+				v.y=(v.y ? v.y : minDY);
+				v.m=(v.m ? v.m : '01');
+				v.d=(v.d ? v.d : '01');
+				v.h=(v.h ? v.h : '00');
+				v.i=(v.i ? v.i : '00');
+				o.data('min',dec(v.y+'-'+v.m+'-'+v.d+' '+v.h+':'+v.i));
+
+				v=getCurrent(o);
+				v.y=(v.y ? v.y : maxDY);
+				v.m=(v.m ? v.m : 12);
+				v.d=(v.d ? v.d : 31);
+				v.h=(v.h ? v.h : 23);
+				v.i=(v.i ? v.i : 59);
+				t.data('max',dec(v.y+'-'+v.m+'-'+v.d+' '+v.h+':'+v.i));
+			}
+
+			if(!ignore || (ignore && !ignore.y)){
+				setY(t);
+				if(o){
+					setY(o);
+				}
+			}
+			if(!ignore || (ignore && !ignore.m)){
+				setM(t);
+				if(o){
+					setM(o);
+				}
+			}
+			if(!ignore || (ignore && !ignore.d)){
+				setD(t);
+				if(o){
+					setD(o);
+				}
+			}
+			if(!ignore || (ignore && !ignore.h)){
+				setH(t);
+				if(o){
+					setH(o);
+				}
+			}
+			if(!ignore || (ignore && !ignore.i)){
+				setI(t);
+				if(o){
+					setI(o);
+				}
+			}
+
+			setValue(t);
+			if(o){
+				setValue(o);
+			}
+		};
+
 		var changeY=function(e){
 			var t=$(e.target).data('target');
-			setY(t);
-			setM(t);
-			setD(t);
-			setValue(t);
+			refresh(t);
 		},
 
 		changeM=function(e){
 			var t=$(e.target).data('target');
-			setM(t);
-			setD(t);
-			setValue(t);
+			refresh(t,{y:true});
 		},
 
 		changeD=function(e){
 			var t=$(e.target).data('target');
-			setValue(t);
+			refresh(t,{y:true,m:true});
 		},
 
 		changeH=function(e){
 			var t=$(e.target).data('target');
-			setValue(t);
+			refresh(t,{y:true,m:true,d:true});
 		},
 
 		changeI=function(e){
 			var t=$(e.target).data('target');
-			setValue(t);
+			refresh(t,{y:true,m:true,d:true,h:true});
 		};
 
-
-		$(this).each(function(){
-			var t=$(this),
-			wrap=$('<span/>'),
+		var ranges=$();
+		var forms=$(this).each(function(){
+			var val,val2,t=$(this),
+			wrap=$('<span class="'+conf.wrapClass+'"/>'),
 			id=(''+Math.random()).substr(2),
 			isTime=t.data('time'),
 			d={
 				id:id,
 				default:dec(t.val()),
-				min:dec(t.data('min')),
-				max:dec(t.data('max'),true),
+				min:t.data('min'),
+				max:t.data('max'),
+				minObj:null,
+				maxObj:null,
 				y:oY.clone(true),
 				m:oM.clone(true),
 				d:oD.clone(true),
 				formatDate:t.data('formatDate') || conf.formatDate,
 				formatTime:null
 			};
+
+			if(d.min){
+				val=dec(d.min);
+				if(!val){
+					d.minObj=d.min;
+					d.min=null;
+					ranges.push(t);
+				}else{
+					d.min=val;
+				}
+			}
+
+			if(d.max){
+				val=dec(d.max);
+				if(!val){
+					d.maxObj=d.max;
+					d.max=null;
+					ranges.push(t);
+				}else{
+					d.max=val;
+				}
+			}
 
 			if(t.data('formatTime')){
 				d.formatTime=t.data('formatTime');
@@ -477,5 +688,32 @@
 
 			t.after(wrap);
 		});
+
+		if(ranges.length){
+			ranges.each(function(i){
+				var t=this,d=t.data(),val;
+				if('string'==typeof d.minObj){
+					val=forms.filter(d.minObj).first();
+					if(val.length){
+						t.data('minObj',val);
+						val.data('maxObj',t);
+						refresh(t);
+					}else{
+						t.data('minObj',null);
+						val.data('maxObj',null);
+					}
+				}else if('string'==typeof d.maxObj){
+					val=ranges.filter(d.maxObj);
+					if(val.length){
+						t.data('maxObj',val);
+						val.data('minObj',t);
+						refresh(t);
+					}else{
+						t.data('maxObj',null);
+						val.data('minObj',null);
+					}
+				}
+			});
+		}
 	};
 })(jQuery);
